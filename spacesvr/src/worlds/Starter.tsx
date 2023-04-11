2e0d18901a420dfb8f432646b968319b7bf22c53
Roberto <roberto.coviello@bearingpoint.com>
2023-04-11 16:56:03 +0700

    files upload

---
+++ spacesvr/src/worlds/Starter.tsx
@@ -0,0 +1,22 @@
+import { Spinning, StandardReality, Model, Interactable } from "spacesvr";
+import TransparentFloor from "ideas/TransparentFloor";
+import {Sky, Cloud} from "@react-three/drei";
+
+export default function Starter() {
+  return (
+    <StandardReality playerProps={{
+      pos: [0,1,1]
+    }}>
+      <ambientLight intensity={4}/>
+      <Model scale={0.007} position-y = {0.01} rotation-x={-Math.PI/2} src = "./vrShowcase.glb"></Model>
+      <Spinning ySpeed={1.5}>
+        <Interactable onClick={() => window.open("https://docs.pmnd.rs/react-three-fiber/getting-started/introduction!")}>
+          <Model scale={0.09} position-y={0.9} position-z={-0.03} src="./react_logo.glb"></Model>
+        </Interactable>
+      </Spinning>
+      <TransparentFloor opacity={0.7} />
+      <Sky sunPosition={[0,1,0]}></Sky>
+      <Cloud position-y={15}></Cloud>
+    </StandardReality>
+  );
+}
